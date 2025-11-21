c1703deef3083e5b6fe1133a06fe276d4f5b78e9
Teeradol <teeradol.gino@gmail.com>
2025-11-21 13:58:41 +0700

    three

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -28,6 +28,7 @@ export default function Login() {
                 headers: {
                     'Content-Type': 'application/json'
                 },
+                credentials: 'include',
                 body: JSON.stringify({
                     email: formData.email,
                     password: formData.password
